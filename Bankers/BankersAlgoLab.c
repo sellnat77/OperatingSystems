c9219c7a9edade44c822e2a280b7388e38c4080c
Russell <sell_nat@yahoo.com>
2015-04-22 20:15:19 -0700

    Working on request method:

--- Bankers/BankersAlgoLab.c
+++ Bankers/BankersAlgoLab.c
@@ -54,26 +54,69 @@ int main(int arg, char* argc[])
 void *consume(void *param)
 {
 	int k;
+	int randArr[3];
 	for( k = 0; k < 3; k++)
 	{
-		
+		randArr[0] = rand()%(avail[0]+1);
+		randArr[1] = rand()%(avail[1]+1);
+		randArr[2] = rand()%(avail[2]+1);
 		sleep(rand()%3);
 		
 		pthread_mutex_lock(&mutex);
-		printf("\n\tHello from Consumer: %d",(int)(pthread_self()));
-		request(1,avail);
+		printf("\n\tHello from Consumer: %d",abs((int)(pthread_self())));
+		request(abs((int)(pthread_self())),randArr);
 		pthread_mutex_unlock(&mutex);
 		
 	}
-	release((int)(pthread_self()));
+	release(abs((int)(pthread_self())));
 }
 
-int request(int A,int B[])
+int request(int A,int B[3])
 {
+	int k,m;
 	int bool = 0;
-	
-	
-	
+	printf("\nCustomer number is: %d",A);
+	printf("\nRequesting %d  %d  %d",B[0],B[1],B[2]);
+	for(m = 0; m < 5; m++)
+	{
+		for(k = 0; k < 3; k++)
+		{
+			bool = 1;
+			if(B[k] <= need[m][k])
+			{
+				if(B[k] <= avail[m][k])
+				{
+					avail[m][k] = avail[m][k]-B[k];
+					allocation[m][k] = allocation[m][k]+B[k];
+					need[m][k] = need[k]-B[k];
+					if(checkSafety(avail) == 0)
+					{
+						
+						printf("failed");
+						avail[m][k] = avail[m][k]+B[k];
+						allocation[m][k] = allocation[m][k]-B[k];
+						need[m][k] = need[m][k]+B[k];
+						bool = 0;
+						break;
+					}
+					else
+					{
+						printf("passed");
+						break;
+					}
+				}
+				else
+				{
+					//wait
+				}
+			}
+			else
+			{
+				break;
+				bool = 0;
+			}
+		}
+	}
 	return(bool);
 }
 
@@ -93,4 +136,19 @@ void populateMax(int limR1,int limR2,int limR3)
 		}
 		printf("\nROne: %2d RTwo: %2d RThree: %2d\n",max[k][0],max[k][1],max[k][2]);
 	}
+}
+
+int checkSafety(int[] arr)
+{
+	int k;
+	for(k = 0; k < 3; k++)
+	{
+
+		if(arr[k] < 0)
+		{
+			return 0;
+		}
+		
+	}
+	return 1;
 }
